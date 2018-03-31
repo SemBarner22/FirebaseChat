01e2c762577ee6ebc055ff28930dfb7a7d45206a
SemBarner22 <ildar1001@mail.ru>
2018-03-31 20:53:05 +0300

    Что-то

---
+++ app/src/main/java/com/sembarner/firebasechat/MainActivity.java
@@ -0,0 +1,23 @@
+package com.sembarner.firebasechat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this, SignupActivity.class));
+        finish();
+    }
+}
